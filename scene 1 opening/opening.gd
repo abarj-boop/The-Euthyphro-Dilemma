faf5e61246a6f2dda26b9660ce50a41c917b7f15
abarj-boop <abarj06@icloud.com>
2025-10-01 11:06:11 -0500

    Add files via upload

---
+++ scene 1 opening/opening.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+const OPENING_DIALOGUE = preload("res://scene 1 opening/opening.dialogue")
+var is_dia_active = true
+# Called w en the node enters the scene tree for the first time.
+func _ready() -> void:
+	DialogueManager.show_dialogue_balloon(OPENING_DIALOGUE)
+	DialogueManager.dialogue_ended.connect(_on_dia_end) 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+
+	
+func _on_dia_end(dialogue):
+	is_dia_active = false
+	if is_dia_active == false:
+		get_tree().change_scene_to_file("res://scene 2 town and maybe temple/town.tscn")
